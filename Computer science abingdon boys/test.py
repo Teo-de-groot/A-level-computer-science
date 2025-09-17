5ca4773e6cc10e358a407666e3bca17fb8251f3e
Teo <teo.degroot07@gmail.com>
2025-09-17 17:41:13 +0100

    hwk

--- Computer science abingdon boys/test.py
+++ Computer science abingdon boys/test.py
@@ -1,40 +1,5 @@
-import random
-#green
-#print("\U0001F7E9")
-#red
-#print("\U0001F7E5")
-Answer=[]
-Attempt=[]
-result=[]
-correct=False
-win_condition=0
-colour_easy=["red","green","blue","orange","yellow"]
-difficulty=input("what difficulty")
-difficulty =difficulty.lower()
-if difficulty == "easy":
-    for i in range (0,5):
-     x = random.randint(0,4)
-     Answer.append(colour_easy[x])
-    while correct!=True:
-        for i in range (0,5):
-            guess=input("colour guess? ")
-            guess=guess.lower()
-            Attempt.append(guess)
-            if Answer[i]==Attempt[i]:
-                result.append("\U0001F7E9")
-                win_condition = win_condition+1
-            else:
-                result.append("\U0001F7E5")
-        print(result)
-        Attempt=[]
-        result=[]
-        if win_condition==5:
-            correct= True
-        else:
-            win_condition=0
-    
-print("You win!")
+import math
+numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 17, 19, 23, 256, -8, -4, -2, 5, -9]
+primes = [number for number in numbers if number > 1 for i in range(2, int(math.sqrt(number)) + 1) if number % i != 0]
+print(primes)
 
-
-        
-            
