d9afec6f2d6a153af37ff45cf0e398bc9fad11ab
Teo <teo.degroot07@gmail.com>
2025-09-17 17:42:14 +0100

    homework but i saved the file this time

--- Computer science abingdon boys/test.py
+++ Computer science abingdon boys/test.py
@@ -1,5 +1,2 @@
-import math
-numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 17, 19, 23, 256, -8, -4, -2, 5, -9]
-primes = [number for number in numbers if number > 1 for i in range(2, int(math.sqrt(number)) + 1) if number % i != 0]
-print(primes)
+
 
