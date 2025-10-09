5c926cda9c1ab7f7bbe0524b8a51510db5289fa3
Teo <teo.degroot07@gmail.com>
2025-10-09 15:15:39 +0100

    fixed OOP & added proper files to .gitignore

--- Computer science abingdon boys/O.O.P/rectangle.py
+++ Computer science abingdon boys/O.O.P/rectangle.py
@@ -1,17 +1,30 @@
-import math
-
-class rectangle:
+class Rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+
     def getarea(self):
+        """Times The Two Values Together"""
         return self.width*self.height
-    def getpermiter(self):
-        return 2*(self.width+self.height)
-    def getdiagonal(self):
-        return math.sqrt(self.width**2+self.height**2)
-a = rectangle(4,3)
-print(a.getarea())
-print(a.getpermiter())
-print(a.getdiagonal())
     
+    def getpermiter(self):
+        """Doubles The Sum Of The Two Entered Values"""
+        return 2*(self.width+self.height)
+    
+    def getdiagonal(self):
+        """Does Pythagarean Theorum To Get Diagonal Length Of A Rectange"""
+        return (self.width**2+self.height**2)**0.5
+    
+a = Rectangle(4,3)
+if a.getarea() == 12:
+    pass 
+else:
+    print("FAILED")
+if a.getpermiter() == 14:
+    pass 
+else:
+    print("FAILED")
+if a.getdiagonal() == 5.0:
+    pass 
+else:
+    print("FAILED")
