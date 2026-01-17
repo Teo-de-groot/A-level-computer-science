180dac03fb795532e963d1d7b8b37a4ec3eae86e
Teo <teo.degroot07@gmail.com>
2026-01-17 15:06:35 +0000

    test

--- Computer science abingdon boys/text-editor-Qt.py
+++ Computer science abingdon boys/text-editor-Qt.py
@@ -1,60 +1,77 @@
 import sys
 import PyQt6.QtWidgets as qtw
 
+
 class Mainwindow(qtw.QMainWindow):
-    def __init__(self, app):
-        super().__init__()
-        self.app = app
-        self.setWindowTitle("Everybody is Kungfu text editing")
+   def __init__(self, app):
+       super().__init__()
+       self.app = app
+       self.setWindowTitle("Everybody is Kungfu text editing")
 
-        layout = qtw.QVBoxLayout()
 
-        self.textedit = qtw.QTextEdit()
-        layout.addWidget(self.textedit)
+       layout = qtw.QVBoxLayout()
 
-        buttons = qtw.QHBoxLayout()
 
-        self.open_button = qtw.QPushButton("Open")
-        self.open_button.clicked.connect(self.open)
+       self.textedit = qtw.QTextEdit()
+       layout.addWidget(self.textedit)
 
-        self.save_button = qtw.QPushButton("Save")
-        self.save_button.clicked.connect(self.save)
 
-        self.quit_button = qtw.QPushButton("Quit")
-        self.quit_button.clicked.connect(self.cancel)
+       buttons = qtw.QHBoxLayout()
 
-        buttons.addStretch()
-        buttons.addWidget(self.open_button)
-        buttons.addWidget(self.save_button)
-        buttons.addWidget(self.quit_button)
-        buttons.addStretch()
 
-        layout.addLayout(buttons)
+       self.open_button = qtw.QPushButton("Open")
+       self.open_button.clicked.connect(self.open)
 
-        widget = qtw.QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
 
-    def cancel(self):
-        self.app.quit()
+       self.save_button = qtw.QPushButton("Save")
+       self.save_button.clicked.connect(self.save)
 
-    def open(self):
-        filename, _ = qtw.QFileDialog.getOpenFileName(
-            self, "Open File", "", "Text Files (*.txt *.html)"
-        )
-        if filename:
-            with open(filename, "r", encoding="utf-8") as file:
-                self.textedit.setPlainText(file.read())
 
-    def save(self):
-        filename, _ = qtw.QFileDialog.getSaveFileName(
-            self, "Save File", "", "Text Files (*.txt *.html)"
-        )
-        if filename:
-            with open(filename, "w", encoding="utf-8") as file:
-                file.write(self.textedit.toPlainText())
+       self.quit_button = qtw.QPushButton("Quit")
+       self.quit_button.clicked.connect(self.cancel)
+
+
+       buttons.addStretch()
+       buttons.addWidget(self.open_button)
+       buttons.addWidget(self.save_button)
+       buttons.addWidget(self.quit_button)
+       buttons.addStretch()
+
+
+       layout.addLayout(buttons)
+
+
+       widget = qtw.QWidget()
+       widget.setLayout(layout)
+       self.setCentralWidget(widget)
+
+
+   def cancel(self):
+       self.app.quit()
+
+
+   def open(self):
+       filename, _ = qtw.QFileDialog.getOpenFileName(
+           self, "Open File", "", "Text Files (*.txt *.html)"
+       )
+       if filename:
+           with open(filename, "r", encoding="utf-8") as file:
+               self.textedit.setPlainText(file.read())
+
+
+   def save(self):
+       filename, _ = qtw.QFileDialog.getSaveFileName(
+           self, "Save File", "", "Text Files (*.txt *.html)"
+       )
+       if filename:
+           with open(filename, "w", encoding="utf-8") as file:
+               file.write(self.textedit.toPlainText())
+
 
 app = qtw.QApplication(sys.argv)
 window = Mainwindow(app)
 window.show()
 app.exec()
+
+
+
